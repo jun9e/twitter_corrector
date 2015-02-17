a1aea965a2de17b590c4dc641526b5d5030e7804
jun9e <mail@elenajung.de>
2015-02-17 23:16:48 +0100

    adding a class to show the last tweet of a twitter search

--- twitter.rb
+++ twitter.rb
@@ -6,14 +6,28 @@ client = Twitter::REST::Client.new do |config|
   config.consumer_secret     = "key2"
 end
 
-puts client.search("railsgirls")
-
-#print question for user
-def ask_twitterhandle
-	puts "Tell me a twitterhandle!"
-	answer = gets.chomp.downcase
+#print question for user and get answer
+def ask_twitterkeyword
+	puts "Tell me a keyowrd you would like to know more on twitter!"
+	gets.chomp.downcase
 end
 
+class TwitterSearch  
+  def initialize(twitter_search_keyword, client) 
+    @twitter_search_keyword = twitter_search_keyword   
+	@client = client
+  end  
+  
+	#get last tweet for given twitter search query
+	def last_tweet
+		result = @client.search(@twitter_search_keyword)
+		result.first.full_text
+	end
+end
+
+puts TwitterSearch.new(ask_twitterkeyword, client).last_tweet
+
+=begin
 tweet_corrector = TweetCorrector.new(client)
 
 # get twitter user name
@@ -22,4 +36,5 @@ tweet_corrector.set_user
 # find latest tweet
 tweet = tweet_corrector.find_last_tweet
 
-puts tweet # Should print tweet
+puts tweet # Should print tweetq12
+=end
